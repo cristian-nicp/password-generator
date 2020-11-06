81aa9eb1afc7d0592c5a6afb8722c04fb2f94683
Cristian Pereira <cristian.nicp@gmail.com>
2020-11-06 17:11:15 -0300

    adding main file .py

---
+++ password_generator.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+import random
+
+message = """   
+    ------------Bienvenido/a al generador de contraseñas aleatorias------------
+"""
+
+
+def password_generator():
+    uppercase = ['A','B','C','D','E','F','G','H','I','J','K','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    lowercase = ['a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    symbols = ['!','$','%','&','=','¡','?','¿','*','+','-','_']
+
+    characters = uppercase + lowercase + numbers + symbols# Generamos una lista que sume simbolos, mayus, minus y núm
+
+    password = [] 
+
+    while len(password) < 19:
+        random_character = random.choice(characters)
+        password.append(random_character)
+    
+    password = ''.join(password)# genero un string de mi lista original,todos los caracteres unidos en una sola cadena
+    return password
+
+
+def run():
+    password = password_generator()
+    print(message)
+    print(f'Tu nueva contraseña es: {password}')
+
+
+if __name__ == "__main__":
+    run()
